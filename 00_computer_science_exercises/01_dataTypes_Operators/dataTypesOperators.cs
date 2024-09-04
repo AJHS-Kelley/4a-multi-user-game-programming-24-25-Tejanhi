c151cf06e30fca58b99126357258e8e279898403
Tejanhi <bigsupreme8@gmail.com>
2024-09-04 14:25:03 -0400

    project merge

---
+++ 00_computer_science_exercises/01_dataTypes_Operators/dataTypesOperators.cs
@@ -0,0 +1,185 @@
+//Data Types and Opeartors, Te'janhi Nester, v0.0
+using System;
+class DataTypeOperators
+  static void Main() {
+   
+  //This is a comment, it gets ignored.
+}
+
+/*
+This is still a comment.
+
+*/
+  // DATA TYPES
+    // C# is STACTICALLY TYPED -- once a variable has a data type, it cannot change.
+
+    // Float - float -- decimal numbers, +/- including 0.0
+
+   // Double - double -- decimal numbers , +/-, including 0.0 < -- MOST COMMON USED DECIMAL
+
+    // Intergers -- int -- whhole numbers, +/-, including 0
+
+    // VARIBLES ARE 'BUCKETS' WITH A NAME TO STORE DATA INSIDE
+
+      // DECLARING (CREATING VARIBLES)
+      // Method #1 -- Declared but not Assign
+      // dataType variableName;
+
+     int myInterger; 
+     double myDouble;
+
+    // Method #2 -- Declare and Assign
+   int myInterger2 = 1;
+   double myDouble2 = 2.05d;
+
+     // Strings -- Collections of leters, numbers, special characters.
+     string myString = "This is my string.";
+
+      // Boolean -- bool - True oe False value
+        bool myBool = true;
+        bool fireReisitance = true;
+        bool hasPurpleKey = false;
+
+       // Character -- char -- a single character.
+       char myChar = 'A'
+
+      // ASSIGNING VALUES AFTER DECLARATION
+
+      myInterger = 5;
+      myInterger = -1.0d;
+
+      //PRINTING VARIABLES TO THE CONSOLES TERMINAL
+    Console.WriteLine(myinterger)
+    Console.WriteLine(myDouble)
+
+   // PRINTING EXPRESSIONS TO THE CONSOLE TERMINAL
+   Console.WriteLine(myInterger -25);
+   Console.WriteLine(myDouble + 2.5d);
+   // Addition
+
+
+
+
+   // OPERATORS -- ARITHMETIC
+
+   ]=-= 
+
+  myNewNumber = myDouble / myInterger;
+
+   //MULTIPLICATION
+   myNewNumber= MyDouble * myInterger;
+
+    // MODULUS -- Divides, then returns the REMAINDER
+    // Most commonly used to determine EVEN or ODD
+    myNewNumber =  = myDouble % 2;
+
+    int students = 6;
+    int numSlices = 36;
+    int leftOvers;
+
+     leftOvers = numSlices % students;
+
+  // Increment / Decrement
+ int x = 0;
+ x++; // What is x equal to now?
+    // Increment ++ adds 1 to the current value and puts the new value into the 
+  x--;
+ // Decrement -- subtracts 1 from the current value
+
+ // Special Assignments Operators
+x += 1;
+x *= 1;
+x /= 1;
+x -= 1;
+x %=1; 
+// take
+
+ // COMPARISON OPERATORS
+
+
+
+
+
+
+
+
+  // NOT-Equal-TO != is
+
+ // // GREATER THAN >
+ // Console.WriteLine(y > z);
+ // // GREATER THAN-OR-EQUAL-TO >=
+//
+
+
+
+
+
+
+int b =1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // 
+
+
+
+
+ Console.WriteLine(b> -1); // True
+Console.WriteLine(!(b > -1)); // False
+
+
+
+ Console.WriteLine('What is your age?  Type it and press ENTER. /n"');
+  string age = Console.ReadLine();
+Console.WriteLine
+int day = 1;
+   switch(day)
+    {
+               case 1:
+                  Console.WriteLine("It's Monday ./n");
+               case 2 :
+                     Console.WriteLine ("It's Tuesday ./n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
