6b05c7b9975e6f611b7cd6ba08e8864264e18d43
Tejanhi <bigsupreme8@gmail.com>
2024-09-10 14:47:14 -0400

    Update strings.cs

--- 00_computer_science_exercises/03_strings/strings.cs
+++ 00_computer_science_exercises/03_strings/strings.cs
@@ -23,4 +23,24 @@ if (greeting.Length > goodbye.Length)
     }
  Console.WriteLine(greeting.ToUpper(); // Make entire string UPPERCASE
  Console.WriteLine(greeting.ToLower()); // Make entire string lowercase
+
+ // String Concatenation "Con-Cat-Uh-Nation"
+ string comboString = greeting + goodbye;
+ Console.WriteLine(comboString);
+
+ // String Concatenation Method #2
+ string comboString2 = string.Concat(greeting, goodbye);
+ Console.WriteLine(comboString2);
+
+ // String Interpolation -- Subsituting variables into strings.
+ string comboString3 = $"My greeting is {greeting} and my goodbye is {goodbye}./n"'
+ Console.WriteLine(comboString3);
+
+ // Substituting Variables into Strings Method 2
+ Console.Writeline("My greeting is" + greeting + "and my goodbye is "+ goodbye + "/n.";
  
+ // Accesing Parts of Strings
+ // index is a specific location of a character in a string
+ // ALL string indexes starts at 0
+ Console.WriteLine(goodbye[0]); // Print the character at index 0.
+ Console.WriteLine(goodbye[4]); // Replace x with what to print the 5th char?
